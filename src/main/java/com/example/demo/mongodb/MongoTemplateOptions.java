5798d93f3e4f30aad6e43ead7ffe48ae33ccb3c1
liusa <liusa@lenovocloud.com>
2019-04-16 19:05:06 +0800

    命名纠正与格式调整

--- src/main/java/com/example/demo/mongodb/MongoTemplateOptions.java
+++ src/main/java/com/example/demo/mongodb/MongoTemplateOptions.java
@@ -30,27 +30,27 @@ public class MongoTemplateOptions {
 		
 		  Query queryOne = Query.query(Criteria.where("id").is("5cb589b40a158532dc8e2998")); 
 		  //mongoTemplate.findById(new ObjectId("5cb589b40a158532dc8e2998"),MongoTesUser.class); 
-		  MongoTesUser user = mongoTemplate.findOne(queryOne,MongoTesUser.class); 
+		  MongoTestUser user = mongoTemplate.findOne(queryOne,MongoTestUser.class); 
 		  System.out.println("----- findOne -----");
 		  System.out.println(user.toString());
 		  
 		  Query queryMany = Query.query(Criteria.where("name").is("刘飒many"));
-		  List<MongoTesUser> users = mongoTemplate.find(queryMany, MongoTesUser.class);
+		  List<MongoTestUser> users = mongoTemplate.find(queryMany, MongoTestUser.class);
 		  System.out.println("----- findMany -----"); System.out.println(users.size());
 		  
 		  
-		  List<MongoTesUser> userList = mongoTemplate.findAll(MongoTesUser.class);
+		  List<MongoTestUser> userList = mongoTemplate.findAll(MongoTestUser.class);
 		  System.out.println("----- findAll -----");
 		  System.out.println(userList.size());
 		  
-		  Query queryLt = Query.query(Criteria.where("age").lt(27)); List<MongoTesUser>
-		  usersLt = mongoTemplate.find(queryLt, MongoTesUser.class);
+		  Query queryLt = Query.query(Criteria.where("age").lt(27)); List<MongoTestUser>
+		  usersLt = mongoTemplate.find(queryLt, MongoTestUser.class);
 		  System.out.println("----- queryLt -----");
 		  System.out.println(usersLt.size());
 		 
 
 		  Query queryRegex = Query.query(Criteria.where("name").regex("liusa"));
-		  List<MongoTesUser> usersRegex = mongoTemplate.find(queryRegex, MongoTesUser.class);
+		  List<MongoTestUser> usersRegex = mongoTemplate.find(queryRegex, MongoTestUser.class);
 		  System.out.println("----- queryRegex -----");
 		  System.out.println(usersRegex.size());
 		  
@@ -58,7 +58,7 @@ public class MongoTemplateOptions {
 		  Query queryOr = Query.query(Criteria.where("").orOperator(
 				    Criteria.where("name").regex("刘飒"),
 				    Criteria.where("age").lt(17)));
-		  List<MongoTesUser> usersOr = mongoTemplate.find(queryOr, MongoTesUser.class);
+		  List<MongoTestUser> usersOr = mongoTemplate.find(queryOr, MongoTestUser.class);
 		  System.out.println("----- queryOr -----");
 		  System.out.println(usersOr.size());
 		
@@ -74,7 +74,7 @@ public class MongoTemplateOptions {
 		
 		//添加数据
 	    for (int i = 0; i < 10; i++) {
-	    	MongoTesUser user = new MongoTesUser();
+	    	MongoTestUser user = new MongoTestUser();
 	    	user.setName("liusa-"+i);
 	    	user.setAge(18+i);
 	    	user.setTags(Arrays.asList("java", "mongodb", "spring"));
@@ -84,15 +84,15 @@ public class MongoTemplateOptions {
 	    
 	    
 	    //批量添加
-	    List<MongoTesUser> userList = new ArrayList<>(10);
+	    List<MongoTestUser> userList = new ArrayList<>(10);
 	    for (int i = 10; i < 20; i++) {
-	    	MongoTesUser user = new MongoTesUser();
+	    	MongoTestUser user = new MongoTestUser();
 	    	user.setName("liusa-"+i);
 	    	user.setAge(18+i);
 	    	user.setTags(Arrays.asList("java", "mongodb", "spring"));
 	    	userList.add(user);
 	    }
-	    mongoTemplate.insert(userList, MongoTesUser.class);
+	    mongoTemplate.insert(userList, MongoTestUser.class);
 	    
 	}
 	
@@ -104,7 +104,7 @@ public class MongoTemplateOptions {
 	 */
 	public void deleteOne(String name) {
 		Query query = Query.query(Criteria.where("name").is(name));
-		MongoTesUser user = mongoTemplate.findAndRemove(query, MongoTesUser.class);
+		MongoTestUser user = mongoTemplate.findAndRemove(query, MongoTestUser.class);
 		System.out.println(user.getName());
 	}
 	
@@ -117,7 +117,7 @@ public class MongoTemplateOptions {
 	 */
 	public void deleteMany(int age) {
 		Query query = Query.query(Criteria.where("age").lt(age));
-		List<MongoTesUser>  list = mongoTemplate.findAllAndRemove(query, MongoTesUser.class);
+		List<MongoTestUser>  list = mongoTemplate.findAllAndRemove(query, MongoTestUser.class);
 		System.out.println(list.size());
 	}
 	
@@ -133,7 +133,7 @@ public class MongoTemplateOptions {
 		Query query = Query.query(Criteria.where("name").is(name));
 //		Query query = Query.query(Criteria.where("id").is("5cb589b40a158532dc8e2995"));
 		Update update = Update.update("name", "刘飒").set("age", 33).set("tags", Arrays.asList("mongodb"));
-		mongoTemplate.updateFirst(query, update, MongoTesUser.class);
+		mongoTemplate.updateFirst(query, update, MongoTestUser.class);
 		
 	}
 	
@@ -145,7 +145,7 @@ public class MongoTemplateOptions {
 	public void updateMany() {
 		Query query = Query.query(Criteria.where("name").is("刘飒many"));
 		Update update = Update.update("name", "刘飒many").set("age", 22).set("tags", Arrays.asList("java","python","mongodb"));
-		mongoTemplate.updateMulti(query, update, MongoTesUser.class);
+		mongoTemplate.updateMulti(query, update, MongoTestUser.class);
 	}
 	
 	
@@ -158,7 +158,7 @@ public class MongoTemplateOptions {
 		Query query = Query.query(Criteria.where("name").is("刘飒many"));
 		Update update = new Update();
 		update.inc("age", 4);
-		mongoTemplate.updateMulti(query, update, MongoTesUser.class);
+		mongoTemplate.updateMulti(query, update, MongoTestUser.class);
 		
 	}
 	
--- src/main/java/com/example/demo/mongodb/MongoTestUser.java
+++ src/main/java/com/example/demo/mongodb/MongoTestUser.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-public class MongoTesUser {
+public class MongoTestUser {
 
 	@Id
 	private String id;
